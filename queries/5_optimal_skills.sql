6dabf906849f3ad1390837aaa185a92c443bea4e
Paul Calzado <pafcalzado@gmail.com>
2024-08-27 12:55:14 +0800

    Add fifth sql query

---
+++ queries/5_optimal_skills.sql
@@ -0,0 +1,22 @@
+/* 
+Answer: What are the most optimal skills to learn (aka it's in high demand and a high-paying skill)?
+- Identify skills in high demand and associated with high average salaries for Data Analyst roles
+- Concentrates on remote positions with specific salaries
+- Why? Targets skills that offer job security (high demand) and financial benefits (high salaries), offering strategic insights for career development in data analysis
+*/
+
+SELECT
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM
+    job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_work_from_home = True
+GROUP BY
+    skills
+ORDER BY
+    demand_count DESC
+LIMIT 5;
